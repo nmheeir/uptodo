ebf997605683fcee870933e09dddac1c15512ddd
nmheeir <nmh7624@gmail.com>
2025-02-15 10:41:56 +0700

    initital commit

---
+++ app/src/main/java/com/kt/uptodo/App.kt
@@ -0,0 +1,16 @@
+package com.kt.uptodo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
